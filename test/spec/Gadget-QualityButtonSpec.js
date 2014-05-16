98699d77d71bd9bd3cb573fffc04d2d51dbc730c
Bruno Pérel <brunoperel@gmail.com>
2014-05-16 11:36:40 +0200

    Couverture de la fonction addQuality()

--- test/spec/Gadget-QualityButtonSpec.js
+++ test/spec/Gadget-QualityButtonSpec.js
@@ -1,9 +1,23 @@
 describe("QualityButton", function() {
 
-    it("should render the radio buttons", function() {
-	    loadFixtures('Gadget-QualityButtonSpec_begin.html');
+	beforeEach(function () {
+		loadFixtures('Gadget-QualityButtonSpec_begin.html');
+	});
 
+    it("should render the radio buttons", function() {
 	    addQualityButtons();
         expect($('#jasmine-fixtures').html().replace(/[\r\n]/g,'')).toEqual(readFixtures('Gadget-QualityButtonSpec_end.html').replace(/[\r\n]/g,''));
     });
+
+	it("should update the quality information", function() {
+		var form = $('form')[0];
+		var textbox = $('[name="wpTextbox1"]');
+
+		addQuality(form, "25%");
+		expect($('[name="wpSummary"]').val()).toEqual('/* Texte incomplet */');
+		expect(textbox.val()).toEqual('{{TextQuality|25%}}');
+
+		addQuality(form, "25%");
+		expect(textbox.val()).toEqual('{{TextQuality|25%}}');
+	});
 });
